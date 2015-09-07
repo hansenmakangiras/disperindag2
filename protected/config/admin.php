03fa78294cbbd63d02b8b6b71f26f484c6889698
hansenmakangiras <hansenmakangiras@gmail.com>
2015-09-08 01:18:26 +0800

    Remove not use file

--- protected/config/admin.php
+++ protected/config/admin.php
@@ -1,26 +1,24 @@
 <?php
 
 return CMap::mergeArray(
-        require(dirname(__FILE__) . '/main.php'), array(
-            'theme' => 'admin',
-            //'defaultController'=>'site',
-            'components' => array(
+    require(dirname(__FILE__) . '/main.php'), array(
+        'theme' => 'admin',
+        'defaultController'=>'site',
+        'components' => array(
+            'urlManager' => array(
+                'urlFormat' => 'path',
+                'showScriptName' => false,
+                'rules' => array(
+                    //'site/login'=>'/',
+                    'admin' => 'admin/site/index',
+                    'admin/user' => 'admin/user/index',
+                    //'admin/berita/index' => 'admin/berita',
 
-                'urlManager' => array(
-                    'urlFormat' => 'path',
-                    'showScriptName' => false,
-                    'rules' => array(
-                        //'site/login'=>'/',
-                        'admin' => 'admin/site/index',
-                        'admin/user' => 'admin/user/index',
-                        //'admin/berita/index' => 'admin/berita',
-
-                        'admin/<_c>' => '<_c>',
-                        'admin/<_c>/<_a>' => '<_c>/<_a>',
-                    ),
+                    'admin/<_c>' => '<_c>',
+                    'admin/<_c>/<_a>' => '<_c>/<_a>',
                 ),
-
-            )
+            ),
         )
+    )
 );
 ?>
